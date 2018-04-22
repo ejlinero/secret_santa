d21ecf7b8d1b8db181b7693a32a4ff2eb2f1ff54
Emilio Jose Linero Garrido <emilio@LexLuthor>
2018-04-22 19:02:16 +0200

    feat: Al menos cuatro miembros para hacer la asignación aleatoria

--- apps/members/models.py
+++ apps/members/models.py
@@ -1,9 +1,16 @@
 from django.db import models
+from .exceptions import InsufficientMemberError
 
 
 class Member(models.Model):
-	name = models.CharField(max_length=16)
-	email = models.EmailField()
+    name = models.CharField(max_length=16)
+    email = models.EmailField(unique=True)
 
-	def __str__(self):
-		return self.name
+    @classmethod
+    def randomly_assinging(cls):
+        member_list = cls.objects.all()
+        if len(member_list) < 4:
+            raise InsufficientMemberError('pass')
+
+    def __str__(self):
+        return self.name
--- apps/members/tests.py
+++ apps/members/tests.py
@@ -1,13 +1,43 @@
+from django.db import IntegrityError
 from django.test import TestCase
 
 from .models import Member
+from .exceptions import InsufficientMemberError
 
 class MemberTestCase(TestCase):
+    
+    @staticmethod
+    def generate_3_members():
+        Member.objects.create(
+            name='Rafael',
+            email='rafael@email.com')
+        Member.objects.create(
+            name='Luisa',
+            email='luisa@email.com')
+        Member.objects.create(
+            name='Ernesto',
+            email='ernesto@email.com')
+    @staticmethod
+    def generate_isabel_member():
+        Member.objects.create(
+            name='Isabel',
+            email='isabel@email.com')
 
-	def test_create_member(self):
-		all_old_members = Member.objects.all().count()
-		Member.objects.create(
-			name='Emilio',
-			email='emilio@email.com') 
-		self.assertEqual(all_old_members + 1, Member.objects.all().count())
+    def test_create_member(self):
+        all_old_members = Member.objects.all().count()
+        Member.objects.create(
+            name='Emilio',
+            email='emilio@email.com') 
+        self.assertEqual(all_old_members + 1, Member.objects.all().count())
 
+    def test_repeated_email(self):
+        self.generate_isabel_member()
+        self.assertRaises(IntegrityError,
+            Member.objects.create,
+            name='isabel',
+            email='isabel@email.com')
+
+    def test_randomly_assinging_whit_3_members_raise_exception(self):
+        self.generate_3_members()
+        self.assertRaises(InsufficientMemberError,
+                          Member.randomly_assinging)
